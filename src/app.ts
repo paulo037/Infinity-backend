2f55c2cadeb3d23149b5ed76a99026b868ee4f55
Paulo <paulo.h.carneiro@ufv.br>
2022-08-26 02:41:52 -0300

    removendo logs com morgan

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 import bodyParser from 'body-parser'
-import morgan from 'morgan'
 
 
 const app = express()
 
-app.use(morgan('dev'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}));
