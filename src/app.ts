99be243147a8d54a2d1c30052c22d3d910ccbf36
Paulo <paulo.h.carneiro@ufv.br>
2022-08-26 02:34:45 -0300

    adicionando logs com morgan

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 import bodyParser from 'body-parser'
-// import morgan from 'morgan'
+import morgan from 'morgan'
 
 
 const app = express()
 
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}));
