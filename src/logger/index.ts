2b1d094ed91674e728b25a247d987cea1ca61840
paulo071825 <paulo.h.carneiro@ufv.br>
2022-12-23 15:19:08 -0300

    change pino config

---
+++ src/logger/index.ts
@@ -0,0 +1,16 @@
+import pino from "pino";
+
+export const logger = pino({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            levelFirst: true,
+            translateTime: 'dd-mm-yyyy HH:MM:ss'
+
+        }
+    }
+
+})
+
